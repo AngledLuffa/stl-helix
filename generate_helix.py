fbc64a7440a9e73ecaab1e839e53961cb2b1aeee
John Bauer <horatio@gmail.com>
2020-02-11 00:22:46 -0800

    Refactor a method to displace by the tube position

--- generate_helix.py
+++ generate_helix.py
@@ -25,6 +25,42 @@ def generate_cube(size):
         for triangle in generate_quad(*side):
             yield triangle
 
+def tube_coordinates(tube_radius, wall_thickness,
+                     start_angle, end_angle, tube_sides,
+                     tube_subdivision, slope_angle, inside, rotation):
+    """
+    Calculate the x,y,z based on the tube position.
+    Arguments have the same meaning as for generate_helix.
+    rotation means how much to rotate the tube.  
+      An unrotated tube will be along the x axis.
+      For a helix, this probably represents where in the helix you are.
+      For a zigzag, you will not want to rotate at all.
+      For an arbitrary curve, you probably want to be normal to the current direction.
+    """
+    tube_angle = start_angle + 360 / tube_sides * tube_subdivision
+    if tube_angle > end_angle:
+        tube_angle = end_angle
+
+    if inside:
+        tube_radius = tube_radius - wall_thickness
+
+    # we will figure out x, y, z as if we had not rotated around the
+    # axis at all.  then we will rotate the resulting vector
+    x_disp = tube_radius * math.cos(tube_angle / 180 * math.pi)
+    vert_disp = -tube_radius * math.sin(tube_angle / 180 * math.pi)
+    # tilt the tube a bit so that things going down the ramp
+    # are going straight when they come out of the ramp
+    y_disp = -vert_disp * math.sin(slope_angle / 180 * math.pi)
+    z_disp =  vert_disp * math.cos(slope_angle / 180 * math.pi)
+
+    r_x_disp = (x_disp * math.cos(rotation / 180 * math.pi) -
+                y_disp * math.sin(rotation / 180 * math.pi))
+    r_y_disp = (x_disp * math.sin(rotation / 180 * math.pi) +
+                y_disp * math.cos(rotation / 180 * math.pi))
+
+    return (r_x_disp, r_y_disp, z_disp)
+
+            
 def coordinates(helix_radius, tube_radius, wall_thickness,
                 start_angle, end_angle,
                 tube_sides, helix_sides,
@@ -48,36 +84,31 @@ def coordinates(helix_radius, tube_radius, wall_thickness,
     location = (0, 0, # move up by the location in the helix
                 tube_radius +
                 vertical_displacement * helix_subdivision / helix_sides)
-    tube_angle = start_angle + 360 / tube_sides * tube_subdivision
-    if tube_angle > end_angle:
-        tube_angle = end_angle
     helix_angle = 360 / helix_sides * helix_subdivision
-
-    if inside:
-        tube_radius = tube_radius - wall_thickness
-
-    # we will figure out x, y, z as if we had not rotated around the
-    # axis at all.  then we will rotate the resulting vector
-    x_disp = helix_radius
-        
-    x_disp = x_disp + tube_radius * math.cos(tube_angle / 180 * math.pi)
-    vert_disp = -tube_radius * math.sin(tube_angle / 180 * math.pi)
-    # tilt the tube a bit so that things going down the ramp
-    # are going straight when they come out of the ramp
-    y_disp = -vert_disp * math.sin(slope_angle / 180 * math.pi)
-    z_disp =  vert_disp * math.cos(slope_angle / 180 * math.pi)
 
     #print("    ", x_disp, vert_disp, y_disp, z_disp)
     #print("    ", helix_angle / 180 * math.pi)
-    r_x_disp = (x_disp * math.cos(helix_angle / 180 * math.pi) -
-                y_disp * math.sin(helix_angle / 180 * math.pi))
-    r_y_disp = (x_disp * math.sin(helix_angle / 180 * math.pi) +
-                y_disp * math.cos(helix_angle / 180 * math.pi))
+    r_x_disp = helix_radius * math.cos(helix_angle / 180 * math.pi)
+    r_y_disp = helix_radius * math.sin(helix_angle / 180 * math.pi)
     #print("    ", r_x_disp, r_y_disp)
 
     location = (location[0] + r_x_disp,
                 location[1] + r_y_disp,
-                location[2] + z_disp)
+                location[2])
+
+    tube_offset = tube_coordinates(tube_radius=tube_radius,
+                                   wall_thickness=wall_thickness,
+                                   start_angle=start_angle,
+                                   end_angle=end_angle,
+                                   tube_sides=tube_sides,
+                                   tube_subdivision=tube_subdivision,
+                                   slope_angle=slope_angle,
+                                   inside=inside,
+                                   rotation=helix_angle)
+
+    location = (location[0] + tube_offset[0],
+                location[1] + tube_offset[1],
+                location[2] + tube_offset[2])
 
     #print("    ", location)
     
