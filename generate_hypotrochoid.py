4e3b4a92ae7ff315195086c1af9c4bc4f44f9e5c
John Bauer <horatio@gmail.com>
2020-02-25 17:06:34 -0800

    Add an option to leave end_t unspecified.  Add a bunch of sample command lines for a 5 lobed flower

--- generate_hypotrochoid.py
+++ generate_hypotrochoid.py
@@ -7,23 +7,52 @@ import marble_path
 
 to make a 3 lobed flower:
 this is the ramp
-python generate_hypotrochoid.py --hypoA 9 --hypoB 3 --hypoC 6 --start_t 1.0472 --end_t 7.3303 --scale 6  --tube_end_angle 240 --slope_angle 12
+python generate_hypotrochoid.py --hypoA 9 --hypoB 3 --hypoC 6 --start_t 1.0472 --scale 6  --tube_end_angle 240 --slope_angle 12
 
 problem: the ramp has some obvious kinks in the corners
 so we can add some regularization
-python generate_hypotrochoid.py --hypoA 9 --hypoB 3 --hypoC 6 --start_t 1.0472 --end_t 7.3303 --scale 10  --tube_end_angle 240 --slope_angle 12 --regularization 0.07
+python generate_hypotrochoid.py --hypoA 9 --hypoB 3 --hypoC 6 --start_t 1.0472 --scale 10  --tube_end_angle 240 --slope_angle 12 --regularization 0.07
 
 complete circle tube.  chop everything except the middle.  this produces the tunnels through the post
-python generate_hypotrochoid.py --hypoA 9 --hypoB 3 --hypoC 6 --start_t 1.0472 --end_t 7.3303 --scale 10  --tube_end_angle 360 --slope_angle 12 --regularization 0.07
+python generate_hypotrochoid.py --hypoA 9 --hypoB 3 --hypoC 6 --start_t 1.0472 --scale 10  --tube_end_angle 360 --slope_angle 12 --regularization 0.07
 
 make this a hole, use it for the negative space in the post
-python generate_hypotrochoid.py --hypoA 9 --hypoB 3 --hypoC 6 --start_t 1.0472 --end_t 7.3303 --scale 10  --tube_end_angle 360 --slope_angle 12 --regularization 0.07  --tube_radius 10.5 --wall_thickness 11
+python generate_hypotrochoid.py --hypoA 9 --hypoB 3 --hypoC 6 --start_t 1.0472 --scale 10  --tube_end_angle 360 --slope_angle 12 --regularization 0.07  --tube_radius 10.5 --wall_thickness 11
 
 
 angle is, not surprisingly, about 60 on the upper connection
 
+TODO:
 to make a 4 lobed flower:
-python generate_hypotrochoid.py --hypoA 12 --hypoB 3 --hypoC 9 --tube_end_angle 240 --slope_angle 8 --scale 4 --start_t 0.7854 --end_t 7.0686
+python generate_hypotrochoid.py --hypoA 12 --hypoB 3 --hypoC 9 --tube_end_angle 240 --slope_angle 8 --scale 4 --start_t 0.7854
+
+5 lobed flower:
+this is a solid tube for the links
+python generate_hypotrochoid.py --hypoA 5 --hypoB 1 --hypoC 4 --tube_end_angle 360 --slope_angle 7.35 --scale 8.5 --start_t 0.6283
+
+height is 136.75
+put it at x=0,y=0,z=15
+the hole in the middle will make it perfect for connecting to the
+  previous piece at 150mm up and the next piece at 18mm
+
+cut off the top of the tube
+python generate_hypotrochoid.py --hypoA 5 --hypoB 1 --hypoC 4 --tube_end_angle 360 --slope_angle 7.35 --scale 8.5 --start_t 0.6283 --end_t 6.2
+z=27.37: still fits
+
+cut off the bottom of the tube as well
+python generate_hypotrochoid.py --hypoA 5 --hypoB 1 --hypoC 4 --tube_end_angle 360 --slope_angle 7.35 --scale 8.5 --start_t 1.2 --end_t 6.2
+
+connector tube will go at x=56,y=62.55
+
+actual ramp:
+python generate_hypotrochoid.py --hypoA 5 --hypoB 1 --hypoC 4 --tube_end_angle 240 --slope_angle 7.35 --scale 8.5 --start_t 0.6283
+this goes at 0,0,15
+
+hole that goes down the middle:
+python generate_hypotrochoid.py --hypoA 5 --hypoB 1 --hypoC 4 --tube_end_angle 360 --slope_angle 7.35 --scale 8.5 --start_t 0.6283  --tube_radius 10.5 --wall_thickness 11
+this goes at 2,2,17
+
+rotation on post: 36 degrees
 """
 
 def get_normal_rotation(theta, x_scale, y_scale, a, b):
@@ -95,6 +124,13 @@ def generate_hypotrochoid(args):
     
     z_t = marble_path.arclength_slope_function(scale_x_t, scale_y_t, args.num_time_steps, args.slope_angle)
     r_t = marble_path.numerical_rotation_function(scale_x_t, scale_y_t)
+
+    min_x = min(scale_x_t(i) for i in range(args.num_time_steps + 1))
+    min_y = min(scale_y_t(i) for i in range(args.num_time_steps + 1))
+    print("Minimum: %f %f" % (min_x, min_y))
+    max_x = max(scale_x_t(i) for i in range(args.num_time_steps + 1))
+    max_y = max(scale_y_t(i) for i in range(args.num_time_steps + 1))
+    print("Maximum: %f %f" % (max_x, max_y))
     
     # can calculate dx & dy like this
     # but when adding regularization, that becomes hideous
@@ -117,9 +153,9 @@ def parse_args():
 
     marble_path.add_tube_arguments(parser, default_slope_angle=12.0)
 
-    parser.add_argument('--hypoA', default=9, type=float,
+    parser.add_argument('--hypoA', default=9, type=int,
                         help='value A in the hypo formula')
-    parser.add_argument('--hypoB', default=3, type=float,
+    parser.add_argument('--hypoB', default=3, type=int,
                         help='value B in the hypo formula')
     parser.add_argument('--hypoC', default=6, type=float,
                         help='value C in the hypo formula')
@@ -133,8 +169,8 @@ def parse_args():
 
     parser.add_argument('--start_t', default=math.pi / 3, type=float,
                         help='Time to start the equation')
-    parser.add_argument('--end_t', default=math.pi * 7 / 3, type=float,
-                        help='Time to start the equation')
+    parser.add_argument('--end_t', default=None, type=float,
+                        help='Time to end the equation.  If None, will be 2pi * b / gcd(a, b)')
     parser.add_argument('--num_time_steps', default=250, type=int,
                         help='Number of time steps in the whole curve')
 
@@ -152,6 +188,11 @@ def parse_args():
 
     args = parser.parse_args()
 
+    if args.end_t is None:
+        N = args.hypoB / math.gcd(args.hypoA, args.hypoB)
+        print("Evaluation time: %d * 2pi" % N)
+        args.end_t = args.start_t + 2 * math.pi * N
+    
     if args.scale is not None:
         args.x_scale = args.scale
         args.y_scale = args.scale
