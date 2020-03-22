6dac2caba993213c168890d3da99def2261462b4
John Bauer <horatio@gmail.com>
2020-03-22 15:27:48 -0700

    Properly handle a single overlap

--- slope_function.py
+++ slope_function.py
@@ -157,6 +157,9 @@ def add_kink_args(parser):
 
 def parse_overlaps(overlap_str):
     overlap_tuple = ast.literal_eval(overlap_str)
+    if (isinstance(overlap_tuple, (tuple, list)) and len(overlap_tuple) == 2 and
+        isinstance(overlap_tuple[0], float) and isinstance(overlap_tuple[1], float)):
+        overlap_tuple = (tuple(overlap_tuple),)
     for i in overlap_tuple:
         if len(i) != 2:
             raise ValueError('Overlaps need to be a tuple of tuples')
