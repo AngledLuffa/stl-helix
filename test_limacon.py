0d020e1fa523d736aa311984ad2299502c95f0f3
John Bauer <horatio@gmail.com>
2020-03-22 10:46:48 -0700

    Refactor setup & teardown

--- test_limacon.py
+++ test_limacon.py
@@ -6,30 +6,27 @@ import unittest
 import generate_limacon
 
 class TestLimacon(unittest.TestCase):
+    def setUp(self):
+        self.test_file = tempfile.NamedTemporaryFile(suffix=".stl", delete=False)
+        self.test_file.close()
+
+    def tearDown(self):
+        os.unlink(self.test_file.name)
+
     def test_basic_limacon(self):
-        test_file = tempfile.NamedTemporaryFile(suffix=".stl", delete=False)
-        test_file.close()
-        try:
-            generate_limacon.main(sys_args=['--output_name', test_file.name,
-                                            '--time_steps', '50',
-                                            '--tube_sides', '32'])
-            self.assertTrue(filecmp.cmp(test_file.name, 'test_files/expected_basic_limacon.stl'))
-        finally:
-            os.unlink(test_file.name)
+        generate_limacon.main(sys_args=['--output_name', self.test_file.name,
+                                        '--time_steps', '50',
+                                        '--tube_sides', '32'])
+        self.assertTrue(filecmp.cmp(self.test_file.name, 'test_files/expected_basic_limacon.stl'))
 
     def test_stretched_limacon(self):
-        test_file = tempfile.NamedTemporaryFile(suffix=".stl", delete=False)
-        test_file.close()
-        try:
-            generate_limacon.main(sys_args=['--output_name', test_file.name,
-                                            '--time_steps', '50',
-                                            '--tube_sides', '32',
-                                            '--length', '200',
-                                            '--cosine_factor', '2.5',
-                                            '--constant_factor', '1.5'])
-            self.assertTrue(filecmp.cmp(test_file.name, 'test_files/expected_stretched_limacon.stl'))
-        finally:
-            os.unlink(test_file.name)
+        generate_limacon.main(sys_args=['--output_name', self.test_file.name,
+                                        '--time_steps', '50',
+                                        '--tube_sides', '32',
+                                        '--length', '200',
+                                        '--cosine_factor', '2.5',
+                                        '--constant_factor', '1.5'])
+        self.assertTrue(filecmp.cmp(self.test_file.name, 'test_files/expected_stretched_limacon.stl'))
 
 if __name__ == '__main__':
     unittest.main()
