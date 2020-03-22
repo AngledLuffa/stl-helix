b19ccc30b0822e239847b53fb0aa79f33be338b4
John Bauer <horatio@gmail.com>
2020-03-22 02:22:07 -0700

    Test a few limacon flags

--- test_limacon.py
+++ test_limacon.py
@@ -17,6 +17,20 @@ class TestLimacon(unittest.TestCase):
         finally:
             os.unlink(test_file.name)
 
+    def test_stretched_limacon(self):
+        test_file = tempfile.NamedTemporaryFile(suffix=".stl", delete=False)
+        test_file.close()
+        try:
+            generate_limacon.main(sys_args=['--output_name', test_file.name,
+                                            '--time_steps', '50',
+                                            '--tube_sides', '32',
+                                            '--length', '200',
+                                            '--cosine_factor', '2.5',
+                                            '--constant_factor', '1.5'])
+            self.assertTrue(filecmp.cmp(test_file.name, 'test_files/expected_stretched_limacon.stl'))
+        finally:
+            os.unlink(test_file.name)
+
 if __name__ == '__main__':
     unittest.main()
 
