b6b5964115309cd0b4302c6138ec52fe280479b8
John Bauer <horatio@gmail.com>
2020-04-23 21:55:08 -0700

    Add the capacity to produce just a single test file

--- regenerate_test_files.py
+++ regenerate_test_files.py
@@ -1,14 +1,27 @@
 import contextlib
 import io
+import sys
 
 from test_generations import TESTS
 
-def main():
+def rebuild(filename):
     for test in TESTS:
-        print("Rebuilding %s" % test.gold_file)
-        args = ['--output_name', test.gold_file] + test.args
-        with contextlib.redirect_stdout(io.StringIO()) as stdout:
+        if test.gold_file == filename or test.gold_file.split("/")[-1] == filename:
+            print("Rebuilding %s" % test.gold_file)
+            args = ['--output_name', test.gold_file] + test.args
             test.model.main(args)
+            
+
+def main():
+    if len(sys.argv) > 1:
+        for filename in sys.argv[1:]:
+            rebuild(filename)
+    else:
+        for test in TESTS:
+            print("Rebuilding %s" % test.gold_file)
+            args = ['--output_name', test.gold_file] + test.args
+            with contextlib.redirect_stdout(io.StringIO()) as stdout:
+                test.model.main(args)
 
 if __name__ == '__main__':
     main()
