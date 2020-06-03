d2cc29e074f179a95ecf306c6b12107bb97de2de
John Bauer <horatio@gmail.com>
2020-06-03 12:43:29 -0700

    Add a description of a piece which goes around the outside of the posts before going back in

--- generate_lissajous.py
+++ generate_lissajous.py
@@ -38,6 +38,20 @@ python generate_lissajous.py --lissajous SUM_HARMONICS --lissA 1 --lissB 0.5 --l
 
 python generate_lissajous.py --lissajous SUM_HARMONICS --lissA 1 --lissB 0.5 --lissC 1 --lissD 0 --lissN 3 --x_scale 87 --y_scale 88.5 --slope_angle 2.9 --start_t 0.11 --end_t 0.39
 
+Lissajous Wraparound
+--------------------------
+
+Goal: make the tube wrap around the outside of the posts instead of going through the middle
+
+Post One: (28.15, 41.86)
+Post Two: (81.76, 164.67)
+
+# Tube
+python generate_lissajous.py --lissajous SUM_HARMONICS --lissA 2 --lissB 0 --lissC 1 --lissD -0.5 --lissN 2.5 --x_scale 55.416 --y_scale 110.832 --slope_angle 3.7 --start_t -0.036 --end_t 1.036 --kink_replace_circle "((0.375, 0.425),(0.575, 0.625))" --tube_radius 12.5 --tube_start_angle "((0.45,-60),(0.55,0))"  --tube_end_angle "((0.45,180),(0.55,240))" --overlaps "((0.09,0.6),(0.4,0.91))" --overlap_separation 24
+
+# Hole for the posts
+python generate_lissajous.py --lissajous SUM_HARMONICS --lissA 2 --lissB 0 --lissC 1 --lissD -0.5 --lissN 2.5 --x_scale 55.416 --y_scale 110.832 --slope_angle 3.7 --start_t -0.036 --end_t 1.036 --kink_replace_circle "((0.375, 0.425),(0.575, 0.625))" --tube_radius 10.5 --wall_thickness 11 --tube_start_angle 0  --tube_end_angle 360 --overlaps "((0.09,0.6),(0.4,0.91))" --overlap_separation 24 
+
 Lissajous Knot
 --------------
 
